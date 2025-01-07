82a3a83508265373ca9d810fd9d6105313c0b671
hyunjun9788 <namhyunjun9788@gmail.com>
2025-01-07 23:57:31 +0900

    feat: 프로젝트 초기세팅

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import prettierConfig from 'eslint-config-prettier';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  prettierConfig,
+];

a7fbfcface9e5f2657f7bc77c11048de70fff38f
hyunjun9788 <namhyunjun9788@gmail.com>
2025-01-08 12:02:28 +0900

    chore: 초기 세팅

--- eslint.config.js
+++ eslint.config.js
@@ -3,8 +3,4 @@ import pluginJs from '@eslint/js';
 import prettierConfig from 'eslint-config-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  prettierConfig,
-];
+export default [{ languageOptions: { globals: globals.browser } }, pluginJs.configs.recommended, prettierConfig];
